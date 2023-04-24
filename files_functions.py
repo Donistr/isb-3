be2456e40a1f92f695a0bf1c8b8200e50a2e5bdf
Donistr <deniselag@mail.ru>
2023-04-24 16:11:12 +0400

    сделал класс FileManager для работы с файлами, добавил аргумент командной строки для использования своего файла с настройками

--- files_functions.py
+++ files_functions.py
@@ -23,113 +23,131 @@ def read_settings(file_name: str = 'settings.json') -> dict:
     return settings
 
 
-def save_asymmetric_private_key(private_key, settings_file_name: str = 'settings.json') -> None:
-    """
-    функция сохраняет приватный ключ для ассиметричного шифрования
-    :param private_key: приватный ключ
-    :param settings_file_name: название файла с настройками
-    :return: ничего
-    """
-    settings = read_settings(settings_file_name)
-    private_key_file_name = settings['secret_key']
-    try:
-        with open(private_key_file_name, 'wb') as private_out:
-            private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
-                                                        format=serialization.PrivateFormat.TraditionalOpenSSL,
-                                                        encryption_algorithm=serialization.NoEncryption()))
-        logging.info(f'Приватный ключ сохранён в файл {private_key_file_name}')
-    except OSError as err:
-        logging.info(f'{err} - ошибка при сохранении приватного ключа {private_key_file_name}')
+class FileManager:
+    def __init__(self, settings_file_name: str = 'settings.json'):
+        settings = read_settings(settings_file_name)
+        self.__initial_file_path = settings['initial_file']
+        self.__encrypted_file_path = settings['encrypted_file']
+        self.__decrypted_file_path = settings['decrypted_file']
+        self.__symmetric_key_path = settings['symmetric_key']
+        self.__nonce_path = settings['nonce']
+        self.__public_key_path = settings['public_key']
+        self.__secret_key_path = settings['secret_key']
 
+    @property
+    def get_initial_file_path(self) -> str:
+        return self.__initial_file_path
 
-def read_asymmetric_private_key(settings_file_name: str = 'settings.json'):
-    """
-    функция считывает сохранённый ранее приватный ключ для ассиметричного шифрования
-    :param settings_file_name: название файла с настройками
-    :return: приватный ключ
-    """
-    settings = read_settings(settings_file_name)
-    private_key_file_name = settings['secret_key']
-    private_key = None
-    try:
-        with open(private_key_file_name, 'rb') as pem_in:
-            private_bytes = pem_in.read()
-        private_key = load_pem_private_key(private_bytes, password=None)
-        logging.info(f'Приватный ключ считан из файла {private_key_file_name}')
-    except OSError as err:
-        logging.info(f'{err} - ошибка при чтении приватного ключа из файла {private_key_file_name}')
-    return private_key
+    @property
+    def get_encrypted_file_path(self) -> str:
+        return self.__encrypted_file_path
 
+    @property
+    def get_decrypted_file_path(self) -> str:
+        return self.__decrypted_file_path
 
-def save_asymmetric_public_key(public_key, settings_file_name: str = 'settings.json') -> None:
-    """
-    функция сохраняет публичный ключ для ассиметричного шифрования
-    :param public_key: публичный ключ
-    :param settings_file_name: название файла с настройками
-    :return: ничего
-    """
-    settings = read_settings(settings_file_name)
-    public_key_file_name = settings['public_key']
-    try:
-        with open(public_key_file_name, 'wb') as public_out:
-            public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
-                                                     format=serialization.PublicFormat.SubjectPublicKeyInfo))
-        logging.info(f'Публичный ключ сохранён в файл {public_key_file_name}')
-    except OSError as err:
-        logging.info(f'{err} - ошибка при чтении публичного ключа из файла {public_key_file_name}')
+    @property
+    def get_symmetric_key_path(self) -> str:
+        return self.__symmetric_key_path
 
+    @property
+    def get_nonce_path(self) -> str:
+        return self.__nonce_path
 
-def read_asymmetric_public_key(settings_file_name: str = 'settings.json'):
-    """
-    функция считывает сохранённый ранее публичный ключ для ассиметричного шифрования
-    :param settings_file_name: название файла с настройками
-    :return: публичный ключ
-    """
-    settings = read_settings(settings_file_name)
-    public_key_file_name = settings['public_key']
-    public_key = None
-    try:
-        with open(public_key_file_name, 'rb') as pem_in:
-            private_bytes = pem_in.read()
-        public_key = load_pem_public_key(private_bytes)
-        logging.info(f'Публичный ключ считан из файла {public_key_file_name}')
-    except OSError as err:
-        logging.info(f'{err} - ошибка при чтении публичного ключа из файла {public_key_file_name}')
-    return public_key
+    @property
+    def get_public_key_path(self) -> str:
+        return self.__public_key_path
 
+    @property
+    def get_secret_key_path(self) -> str:
+        return self.__secret_key_path
 
-def read_text(file_name: str, settings_file_name: str = 'settings.json') -> bytes:
-    """
-    функция считывает текстовый файл
-    :param file_name: название файла
-    :param settings_file_name: название файла с настройками
-    :return: текст из файла
-    """
-    settings = read_settings(settings_file_name)
-    text_file_name = settings[file_name]
-    text = None
-    try:
-        with open(text_file_name, mode='rb') as text_file:
-            text = text_file.read()
-        logging.info(f'Файл {text_file_name} прочитан')
-    except OSError as err:
-        logging.info(f'{err} - ошибка при чтении файла {text_file_name}')
-    return text
+    def save_asymmetric_private_key(self, private_key) -> None:
+        """
+        метод сохраняет приватный ключ для ассиметричного шифрования
+        :param private_key: приватный ключ
+        :return: ничего
+        """
+        try:
+            with open(self.__secret_key_path, 'wb') as private_out:
+                private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                            format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                            encryption_algorithm=serialization.NoEncryption()))
+            logging.info(f'Приватный ключ сохранён в файл {self.__secret_key_path}')
+        except OSError as err:
+            logging.info(f'{err} - ошибка при сохранении приватного ключа {self.__secret_key_path}')
 
+    def read_asymmetric_private_key(self):
+        """
+        метод считывает сохранённый ранее приватный ключ для ассиметричного шифрования
+        :return: приватный ключ
+        """
+        private_key = None
+        try:
+            with open(self.__secret_key_path, 'rb') as pem_in:
+                private_bytes = pem_in.read()
+            private_key = load_pem_private_key(private_bytes, password=None)
+            logging.info(f'Приватный ключ считан из файла {self.__secret_key_path}')
+        except OSError as err:
+            logging.info(f'{err} - ошибка при чтении приватного ключа из файла {self.__secret_key_path}')
+        return private_key
 
-def write_text(text: bytes, file_name: str, settings_file_name: str = 'settings.json') -> None:
-    """
-    функция записывает текст в файл
-    :param text: текст
-    :param file_name: название файла
-    :param settings_file_name: название файла с настройками
-    :return: ничего
-    """
-    settings = read_settings(settings_file_name)
-    text_file_name = settings[file_name]
-    try:
-        with open(text_file_name, mode='wb') as text_file:
-            text_file.write(text)
-        logging.info(f'Текст записан в файл {text_file_name}')
-    except OSError as err:
-        logging.info(f'{err} - ошибка при записи в файл {text_file_name}')
+    def save_asymmetric_public_key(self, public_key) -> None:
+        """
+        метод сохраняет публичный ключ для ассиметричного шифрования
+        :param public_key: публичный ключ
+        :return: ничего
+        """
+        try:
+            with open(self.__public_key_path, 'wb') as public_out:
+                public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                         format=serialization.PublicFormat.SubjectPublicKeyInfo))
+            logging.info(f'Публичный ключ сохранён в файл {self.__public_key_path}')
+        except OSError as err:
+            logging.info(f'{err} - ошибка при чтении публичного ключа из файла {self.__public_key_path}')
+
+    def read_asymmetric_public_key(self):
+        """
+        метод считывает сохранённый ранее публичный ключ для ассиметричного шифрования
+        :return: публичный ключ
+        """
+        public_key = None
+        try:
+            with open(self.__public_key_path, 'rb') as pem_in:
+                private_bytes = pem_in.read()
+            public_key = load_pem_public_key(private_bytes)
+            logging.info(f'Публичный ключ считан из файла {self.__public_key_path}')
+        except OSError as err:
+            logging.info(f'{err} - ошибка при чтении публичного ключа из файла {self.__public_key_path}')
+        return public_key
+
+    @staticmethod
+    def read_text(file_path: str) -> bytes:
+        """
+        метод считывает текстовый файл
+        :param file_path: путь к файлу
+        :return: текст из файла
+        """
+        text = None
+        try:
+            with open(file_path, mode='rb') as text_file:
+                text = text_file.read()
+            logging.info(f'Файл {file_path} прочитан')
+        except OSError as err:
+            logging.info(f'{err} - ошибка при чтении файла {file_path}')
+        return text
+
+    @staticmethod
+    def write_text(text: bytes, file_path: str) -> None:
+        """
+        метод записывает текст в файл
+        :param text: текст
+        :param file_path: путь к файлу
+        :return: ничего
+        """
+        try:
+            with open(file_path, mode='wb') as text_file:
+                text_file.write(text)
+            logging.info(f'Текст записан в файл {file_path}')
+        except OSError as err:
+            logging.info(f'{err} - ошибка при записи в файл {file_path}')
--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 from asymmetric import generate_asymmetric_keys, encrypt_asymmetric, decrypt_asymmetric
 from symmetric import generate_symmetric_keys, encrypt_symmetric, decrypt_symmetric
-from files_functions import read_settings, save_asymmetric_private_key, read_asymmetric_private_key, read_text, \
-    write_text
+from files_functions import read_settings, FileManager
 import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('-set', '--settings', type=str,
+                        help='Использовать собственный файл с настройками (Указать путь к файлу)')
     program_mode_group = parser.add_mutually_exclusive_group(required=True)
     program_mode_group.add_argument('-gen', '--generation', help='Запускает режим генерации ключей')
     program_mode_group.add_argument('-enc', '--encryption', help='Запускает режим шифрования')
     program_mode_group.add_argument('-dec', '--decryption', help='Запускает режим дешифрования')
     args = parser.parse_args()
+    if args.settings:
+        file_manager = FileManager(args.settings)
+    else:
+        file_manager = FileManager()
     if read_settings():
         if args.generation:
             symmetric_key, nonce = generate_symmetric_keys()
             private_key, public_key = generate_asymmetric_keys()
-            save_asymmetric_private_key(private_key)
+            file_manager.save_asymmetric_private_key(private_key)
             encrypted_symmetric_key = encrypt_asymmetric(symmetric_key, public_key)
             encrypted_nonce = encrypt_asymmetric(nonce, public_key)
-            write_text(encrypted_symmetric_key, 'symmetric_key')
-            write_text(encrypted_nonce, 'nonce')
+            file_manager.write_text(encrypted_symmetric_key, file_manager.get_symmetric_key_path)
+            file_manager.write_text(encrypted_nonce, file_manager.get_nonce_path)
         elif args.encryption:
-            private_key = read_asymmetric_private_key()
-            encrypted_symmetric_key = read_text('symmetric_key')
-            encrypted_nonce = read_text('nonce')
+            private_key = file_manager.read_asymmetric_private_key()
+            encrypted_symmetric_key = file_manager.read_text(file_manager.get_symmetric_key_path)
+            encrypted_nonce = file_manager.read_text(file_manager.get_nonce_path)
             symmetric_key = decrypt_asymmetric(encrypted_symmetric_key, private_key)
             nonce = decrypt_asymmetric(encrypted_nonce, private_key)
-            text = read_text('initial_file')
+            text = file_manager.read_text(file_manager.get_initial_file_path)
             encrypted_text = encrypt_symmetric(text, symmetric_key, nonce)
-            write_text(encrypted_text, 'encrypted_file')
+            file_manager.write_text(encrypted_text, file_manager.get_encrypted_file_path)
         elif args.decryption:
-            private_key = read_asymmetric_private_key()
-            encrypted_symmetric_key = read_text('symmetric_key')
-            encrypted_nonce = read_text('nonce')
+            private_key = file_manager.read_asymmetric_private_key()
+            encrypted_symmetric_key = file_manager.read_text(file_manager.get_symmetric_key_path)
+            encrypted_nonce = file_manager.read_text(file_manager.get_nonce_path)
             symmetric_key = decrypt_asymmetric(encrypted_symmetric_key, private_key)
             nonce = decrypt_asymmetric(encrypted_nonce, private_key)
-            encrypted_text = read_text('encrypted_file')
+            encrypted_text = file_manager.read_text(file_manager.get_encrypted_file_path)
             decrypted_text = decrypt_symmetric(encrypted_text, symmetric_key, nonce)
-            write_text(decrypted_text, 'decrypted_file')
+            file_manager.write_text(decrypted_text, file_manager.get_decrypted_file_path)
